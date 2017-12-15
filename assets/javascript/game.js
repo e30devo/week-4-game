f81d8520a9caec96cb87eec0914a68562200c8f9
e30devo <dthomase30@gmail.com>
2017-12-15 15:34:47 -0800

    Initial index.html base style.css base game.js

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    //assign onclick handlers for each crystal
+    $(".crystal-1").on("click", function() {
+        $(".crystal-1").hide(); //testing onclick functionality
+    });
+    $(".crystal-2").on("click", function() {
+        $(".crystal-2").hide(); //testing onclick functionality
+    });
+    $(".crystal-3").on("click", function() {
+        $(".crystal-3").hide(); //testing onclick functionality
+    });
+    $(".crystal-4").on("click", function() {
+        $(".crystal-4").hide(); //testing onclick functionality
+    });
+
+
+
+
+}); //close for document.ready
